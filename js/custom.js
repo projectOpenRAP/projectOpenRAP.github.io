7dbd11dee9f67cc427f8e8893f6a6d1502fa29b0
“Sandip <sandip_p@tekditechnologies.com>
2018-11-12 16:52:29 +0530

    Issue #000 task: Initial commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		//navText: ["<img src='img/Rectangle_4.png'>","<img src='img/Rectangle_4 (1).png'>"],
+		//navText: ["<img src='img/Layer_14.png'>","<img src='img/Layer_14 (1).png'>"],
+		items:1		
+	});
+});
